bdeefa3f218ce4f1a26cc32c0b3862966ec9915d
patranger <103427233+patranger@users.noreply.github.com>
2022-07-11 13:40:01 +0200

    Delete Development directory

--- Development/cake.ps1
+++
@@ -1,4 +0,0 @@
-New-Cake -Name "plumberd" -Root "../"
-
-Add-CakeStep -Name "Build All" -Action {  Invoke-DotnetBuild -All  }
-Add-CakeStep -Name "Publish to nuget.org" -Action { Invoke-NugetPublish -SourceUrl "https://nuget.org" }
