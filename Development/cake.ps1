d93df1a73fd0380d4118c17e9706a39d36748011
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2022-07-11 12:06:12 +0200

    cake-srcipt

--- Development/cake.ps1
+++ Development/cake.ps1
@@ -1,1 +1,4 @@
-Write-Host "Hello Plumberd"
+New-Cake -Name "plumberd" -Root "../"
+
+Add-CakeStep -Name "Build All" -Action {  Invoke-DotnetBuild -All  }
+Add-CakeStep -Name "Publish to nuget.org" -Aciton { Invoke-NugetPublish -SourceUrl "https://nuget.org" }
