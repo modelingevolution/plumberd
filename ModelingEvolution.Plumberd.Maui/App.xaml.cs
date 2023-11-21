c02737402e902c1675f00f9d397c27ada000c50c
Daniel Robaszkiewicz <daniel.robaszkiewicz@modelingevolution.com>
2023-11-21 14:05:37 +0100

    Maui added

---
+++ ModelingEvolution.Plumberd.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ModelingEvolution.Plumberd.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ModelingEvolution.Plumberd.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ModelingEvolution.Plumberd.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
